3ca6871864da76ffa9e416bc48322c1858596ff0
soffolk <zlx.star@gmail.com>
2013-02-05 09:53:31 +0800

    modify generators command

--- lib/generators/left_side/install_generator.rb
+++ lib/generators/left_side/install_generator.rb
@@ -1,8 +1,8 @@
 module LeftSide
   module Generators
-    class InstallGenerator < Rails::Generators::Base
+    class InstallGenerator < ::Rails::Generators::Base
       desc "copy config file"
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
 
       def copy_config
         copy_file "config/section.yml.example", "config/left_side/section.yml"
